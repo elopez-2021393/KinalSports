bf4418d3b91b2cd126d0fba1fd02a2e11d5514d8
elopez-2021393 <elopez-2021393@kinal.edu.gt>
2026-04-23 09:07:28 -0600

    Arreglar confirmModal (ora si)

--- client-admin/src/app/App.jsx
+++ client-admin/src/app/App.jsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { AppRoutes } from './routes/AppRoutes.jsx'
 import { useAuthStore } from '../features/auth/store/authStore.js'
+import { UiConfirmHost } from '../features/auth/components/ConfirmModal.jsx'
 
 export const App = () => {
   const checkAuth = useAuthStore((state) => state.checkAuth);
@@ -25,7 +26,9 @@ export const App = () => {
           }
         }}
       />
+      <UiConfirmHost />
       <AppRoutes />
     </>
   )
 }
+
